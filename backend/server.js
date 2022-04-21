be613a000eee1e16862d2d36f50e1ee28c356ee5
vishnu <vishnukumaran257@gmail.com>
2022-04-21 16:50:36 +0530

    again

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express()
  app.use(express.json())
  app.use(express.urlencoded({ extended: false}))
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+
 app.use('/api/blogs', require('./routes/blogRoutes'))
 
 
